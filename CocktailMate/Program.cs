cb0a64982360e0d4da95888ebe14e546685a356a
iainuk86 <iainuk1986@gmail.com>
2024-04-13 18:25:05 +1000

    Add CORS configuration

--- CocktailMate/Program.cs
+++ CocktailMate/Program.cs
@@ -3,11 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("AppDbContext")));
 
 var app = builder.Build();
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
